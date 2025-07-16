ced78c75e3ce7d99f5ed702447cc6891da24bcb7
Khadija Muhammad Tasiu <dignifiedmt@gmail.com>
2025-07-16 10:34:57 +0100

    new update

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,15 @@
-// // import { defineConfig } from 'vite'
-// // import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// // // https://vite.dev/config/
-// // export default defineConfig({
-// //   plugins: [react()],
-// // })
-// import {defineConfig} from "vite";
-// import react from "@vitejs/plugin-react";
-// import {vitePluginGhPages} from "vite-plugin-gh-pages";
-
+// // https://vite.dev/config/
 // export default defineConfig({
-//     plugins: [react(), vitePluginGhPages()],
-//     base: "/dignity-health/",
-// });
+//   plugins: [react()],
+// })
+
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/dignity-health/",
+    base: "",
 });
